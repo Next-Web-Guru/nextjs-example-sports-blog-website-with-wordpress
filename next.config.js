72898077de15c500d26825c6e3e2e7f123aff0ae
santosh <santoshdevnath15@gmail.com>
2022-03-04 17:52:37 +0530

    pwa installed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
     env: {
         wordpressApiUrl: "https://babacric.in/graphql",
         headerMenuName: "BabaCrick Main Menu"
@@ -6,4 +8,4 @@ module.exports = {
     images: {
         domains: ['i0.wp.com', 'secure.gravatar.com', 'babacricnews.s3.ap-south-1.amazonaws.com'],
     },
-};
+})
--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,11 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <meta charSet="UTF-8" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="manifest" href="/pwa/manifest.json" />
+        <link rel="apple-touch-icon" href="/pwa/ios/50.png" />
+        <meta name='mobile-web-app-capable' content='yes' />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
         <meta data-rh="true" name="google-site-verification" content="3FCgkBGO8Qfael6rkqbx7be0JHNduxct6Xc7TFsc5_0" />
         <meta data-rh="true" name="facebook-domain-verification" content="vylwdr385iepjfnp4mkn0s77p6p94b" />
 
