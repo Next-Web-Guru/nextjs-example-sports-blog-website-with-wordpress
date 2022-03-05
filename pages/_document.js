a247c9e287f1d544b49b3e2ab4bcaf816d1dbbfb
santosh <santoshdevnath15@gmail.com>
2022-03-05 14:48:32 +0530

    analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,18 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html:
+                `(function(w,l){` +
+                `w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});` +
+                `})(window,'dataLayer');`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
